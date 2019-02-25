85886c96f36522024dc9d6245c30de7faaf309d4
xh <zhanghaoran@xigua.city>
2019-02-25 17:59:40 +0800

    优化

--- airport/ruby/index.rb
+++ airport/ruby/index.rb
@@ -1,21 +1,20 @@
 require './platform'
 require './passenger'
 
+print "你的乘坐类型: "
+type = gets
+
 print "输入你的出发时间: "
 from_time = gets
-print "你的乘坐类型: "
-from_type = gets
 
 print "输入你的返回时间: "
 to_time = gets
-print "你的乘坐类型: "
-to_type = gets
 
-passenger_from = Passenger.new from_time, from_type
-platform_from = Platform.new 'Xian'
+passenger_from = Passenger.new from_time, type
+platform_from = Platform.new 'Chengdu'
 
-passenger_to = Passenger.new from_time, to_type
-platform_to = Platform.new 'Chengdu'
+passenger_to = Passenger.new from_time, type
+platform_to = Platform.new 'Xian'
 
 trickets_from = platform_from.trip(passenger_from.take_time, passenger_from.vip?)
 trickets_to = platform_to.trip(passenger_to.take_time, passenger_to.vip?)
